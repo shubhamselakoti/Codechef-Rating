7541f1c4ba43721f0676f09230af07a8dcba3740
Shubham Selakoti <86091766+shubhamselakoti@users.noreply.github.com>
2024-05-15 21:14:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,6 +44,8 @@ app.get("/:username", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3001;
+}
+app.listen(port);
